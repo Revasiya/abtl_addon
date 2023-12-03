197d30db68a5a43c21a949abee116f903bf80fd4
InshaSiS <136191740+Inshasis@users.noreply.github.com>
2023-12-03 18:20:40 +0530

    feat: Add New Doctype

---
+++ abtl_addon/abtl_addon/doctype/doctype/speed/test_speed.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, envisionx Oman and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSpeed(FrappeTestCase):
+	pass
